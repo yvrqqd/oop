20d37b597d7c1ee71b65d44f06ac086d94b4bba6
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-09-06 19:38:44 +0700

    Create count_words.cpp

---
+++ Task_0/count_words.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <fstream>
+#include <regex>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    if (argc < 2)
+        return 1;
+
+    const regex pattern("[a-zA-Z0-90]+");
+    const string filename_in = argv[1];
+    const string filename_out = argv[2];
+
+    int counter = 0;
+    string tmp_line;
+    unordered_map<string, int> words;
+    ifstream file_in(filename_in);
+
+    if (file_in.is_open()) {
+        while (getline(file_in, tmp_line)) {
+            sregex_iterator it = sregex_iterator(tmp_line.begin(), tmp_line.end(), pattern);
+            while (it != sregex_iterator()) {
+                auto coincidence = words.find((*it).str());
+                if (coincidence != words.end())
+                    coincidence->second += 1;
+                else
+                    words.insert({(*it).str(), 1});
+                counter++;
+                it++;
+            }
+        }
+    }
+
+    vector<pair<string, int>> vectorized;
+    vectorized.reserve(words.size());
+    for (const auto &word: words)
+        vectorized.emplace_back(word);
+
+    sort(vectorized.begin(), vectorized.end(), [](auto &left, auto &right) {
+        return left.second > right.second;
+    });
+
+    ofstream file_out(filename_out);
+    if (file_in.is_open())
+        for (const auto &elem: vectorized)
+            file_out << elem.first << "," << elem.second << ","
+                     << (double) elem.second / counter << "\n";
+
+    return 0;
+}
