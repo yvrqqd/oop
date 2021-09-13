01b6daccf4bde31ac9b6bb4cc01d64f0bc95f4c9
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-09-13 09:32:07 +0700

    Update count_words.cpp

--- Task_0/count_words.cpp
+++ Task_0/count_words.cpp
@@ -19,18 +19,19 @@ int main(int argc, char **argv) {
     unordered_map<string, int> words;
     ifstream file_in(filename_in);
 
-    if (file_in.is_open()) {
-        while (getline(file_in, tmp_line)) {
-            sregex_iterator it = sregex_iterator(tmp_line.begin(), tmp_line.end(), pattern);
-            while (it != sregex_iterator()) {
-                auto coincidence = words.find((*it).str());
-                if (coincidence != words.end())
-                    coincidence->second += 1;
-                else
-                    words.insert({(*it).str(), 1});
-                counter++;
-                it++;
-            }
+    if (not file_in.is_open())
+        return 1;
+    
+    while (getline(file_in, tmp_line)) {
+        sregex_iterator it = sregex_iterator(tmp_line.begin(), tmp_line.end(), pattern);
+        while (it != sregex_iterator()) {
+            auto coincidence = words.find((*it).str());
+            if (coincidence != words.end())
+                coincidence->second += 1;
+            else
+                words.insert({(*it).str(), 1});
+            counter++;
+            it++;
         }
     }
 
