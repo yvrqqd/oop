ea83bed098f8b78d2caee2f28a12daf1dd817f90
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-10-03 23:22:08 +0700

    Update and rename trit.cpp to TritSet.cpp

---
+++ Task_1/TritSet.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include "TritSet.h"
+
+TrisSet::vector<unsigned int> arr;
+
+void TritSet::capacity() {
+
+}
+
+void TritSet::shrink() {
+
+}
+
+void TritSet::cardinality(Trit value) {
+    //std::unordered_map<Trit, int, std::hash<int> > cardinality();
+}
+
+size_t TritSet::trim(size_t lastIndex) {
+
+}
+
+size_t TrisSet::length();
+
+}
