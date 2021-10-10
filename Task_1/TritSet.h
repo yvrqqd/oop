ba04c0e179c5dcdc2c81502a644cd35eb2b45cb2
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-10-11 02:17:23 +0700

    Update TritSet.h

--- Task_1/TritSet.h
+++ Task_1/TritSet.h
@@ -1,19 +1,37 @@
 #pragma once
-#include <vector>
 
-enum class Trit : unsigned int {
-    False,
-    Unknown,
-    True
-};
+typedef unsigned int uint;
 
-class TritSet{
-private:
-    vector<unsigned int> arr;
-public:
-    void capacity();
-    void shrink();
-    void cardinality(Trit value);
-    size_t trim(size_t lastIndex);
-    size_t length();
-};
+namespace tritset {
+
+    enum class Trit {
+        False = -1, Unknown, True
+    };
+    class TritProxy {
+    private:
+
+    public:
+        TritProxy(tritset tritSet);
+        bool operator==(Trit trit) const;
+    };
+
+    class TritSet {
+    private:
+        std::size_t length;
+        std::size_t* data;
+    public:
+        TritSet(std::size_t i);
+        std::size_t capacity();
+        const Trit operator[](std::size_t idx) const;
+        const Trit operator&(std::size_t idx) const;
+        const Trit operator|(std::size_t idx) const;
+        const Trit operator!(std::size_t idx) const;
+        void shrink();
+
+        void cardinality(Trit value);
+
+        std::size_t trim(size_t lastIndex);
+
+        std::size_t length();
+    };
+}
