38c401e19b3214aebd7495ad388c4f94a16b4655
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-10-25 00:26:05 +0700

    Update TritSet.h

--- Task_1/TritSet.h
+++ Task_1/TritSet.h
@@ -3,13 +3,11 @@
 typedef unsigned int uint;
 
 namespace tritset {
-
     enum class Trit {
         False = -1, Unknown, True
     };
-    class TritProxy {
-    private:
 
+    class TritProxy {
     public:
         TritProxy(tritset tritSet);
         bool operator==(Trit trit) const;
@@ -18,20 +16,28 @@ namespace tritset {
     class TritSet {
     private:
         std::size_t length;
-        std::size_t* data;
+        std::size_t *data;
     public:
-        TritSet(std::size_t i);
-        std::size_t capacity();
-        const Trit operator[](std::size_t idx) const;
-        const Trit operator&(std::size_t idx) const;
-        const Trit operator|(std::size_t idx) const;
-        const Trit operator!(std::size_t idx) const;
-        void shrink();
-
-        void cardinality(Trit value);
+        TritSet(std::size_t x);
 
         std::size_t trim(size_t lastIndex);
 
         std::size_t length();
+
+        std::size_t capacity();
+
+        srd::size_t cardinality(Trit value);
+
+        std::unordered_map<Trit, int, std::hash<int>> cardinality();
+
+        void shrink();
+
+        const Trit operator[](std::size_t idx) const;
+
+        const Trit operator&(Trit a, Trit b) const;
+
+        const Trit operator|(Trit a, Trit b) const;
+
+        const Trit operator!(Trit a) const;
     };
 }
