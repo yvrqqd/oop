e4b73a86d898d5c3e30a7bb41026647a025fbf76
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-12-06 00:10:56 +0700

    Update Trit.h

--- Task_1/Trit.h
+++ Task_1/Trit.h
@@ -5,4 +5,4 @@ enum class Trit {False = -1, Unknown, True};
 Trit operator&(Trit t1, Trit t2);
 Trit operator|(Trit t1, Trit t2);
 Trit operator!(Trit t1);
-std::ostream& operator<<(std::ostream& os, trit t);
+std::ostream& operator<<(std::ostream& os, Trit t);
