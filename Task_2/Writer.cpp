0af9630a9221e5404b36af7dd40ac7ed2600dd6c
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-12-27 06:45:26 +0700

    Update Writer.cpp

--- Task_2/Writer.cpp
+++ Task_2/Writer.cpp
@@ -1,11 +1,10 @@
 #include "Writer.h"
 
-Writer::WriteFile(string out) {
+Writer::WriteFile(std::string out) {
     args.push_back(out);
 }
 
-
-bool Writer::exec(vector<string> *input, vector<string> *output) {
+bool Writer::exec(std::vector<std::string> *input, std::vector<std::string> *output) {
     ofstream stream(args[0]);
     for (auto &i : *input){
         stream << i <<endl;
