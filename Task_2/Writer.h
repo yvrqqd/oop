db8d39fcd05ef15016ffadeab4fa3bfc87b47745
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-12-27 06:41:58 +0700

    Update Writer.h

--- Task_2/Writer.h
+++ Task_2/Writer.h
@@ -1,1 +1,10 @@
+#pragma once
+#include "Worker.h"
 
+class Writer: public Worker{
+private:
+    vector <string> args;
+public:
+    WriteFile(string out);
+    bool exec(vector<string> *input, vector<string> *output) override;
+};
