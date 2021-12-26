5be5d754f9a2e321cd3a00b69ce290de3ecb953c
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-12-27 06:48:05 +0700

    Update Validator.h

--- Task_2/Validator.h
+++ Task_2/Validator.h
@@ -1,1 +1,12 @@
+#pragma once
 
+#include <string>
+#include <sys/stat.h>
+#include <stdexcept>
+
+class Validator
+{
+public:
+    bool file_exists(const std::string& file);
+    void file_error(const std::string& message);
+};
