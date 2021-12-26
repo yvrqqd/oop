09778aaf2ddd04853e9b625e863f0b971542d264
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-12-27 06:50:08 +0700

    Update Validator.cpp

--- Task_2/Validator.cpp
+++ Task_2/Validator.cpp
@@ -1,1 +1,9 @@
+#include "Validator.h"
 
+bool Validator::file_exists(const std::string& File) {
+    struct stat buf{};
+    return (stat (File.c_str(), &buf) == 0);
+}
+void Validator::file_error(const std::string& message) {
+    throw invalid_argument("Error in file " + message);
+}
