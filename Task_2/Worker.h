9f20c010a4bd62444b46c6dbdb530e0853b5bd37
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-12-27 06:43:55 +0700

    Update Worker.h

--- Task_2/Worker.h
+++ Task_2/Worker.h
@@ -1,1 +1,11 @@
+#pragma once
 
+#include <vector>
+#include <string>
+#include "Validator.cpp"
+
+class Worker {
+public:
+    virtual ~Worker() = default;
+    virtual bool exec(std::vector <std::string> *input, std::vector <std::string> * output) = 0;
+};
