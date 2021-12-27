bd3ff4331959286bd7b9d297071bc2d7277f9f8d
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-12-27 07:28:13 +0700

    Create ReplaceBlock.cpp

---
+++ Task_2/ReplaceBlock.cpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Replace.h"
+
+ReplaceBlock::Replace(const string& word1, const string& word2) {
+    args.push_back(word1);
+    args.push_back(word2);
+}
+
+bool ReplaceBlock::exec(std::vector<std::string> *input, std::vector<std::string> *output,
+                        bool &contain_input, bool &contain_output, unsigned int ind) {
+    if(!contain_input)
+        throw logic_error("No input on replace, Block index: " + to_string(ind));
+
+    (*output) = (*input);
+    if(input->empty())
+        return true;
+
+    size_t index;
+    for(string &i : *output)
+        while ((index = i.find(args[0])) != std::string::npos)
+            i.replace(index, args[0].size(), args[1]);
+
+    contain_output = true;
+    return true;
+}
