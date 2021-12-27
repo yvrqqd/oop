b3f75c4413b245d4472e98ce7b2b9084f23ea37c
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-12-27 08:12:33 +0700

    Update Reader.h

--- Task_2/Reader.h
+++ Task_2/Reader.h
@@ -5,7 +5,7 @@
 
 class Reader:public Worker {
 private:
-    std::vector <std::string> args;
+    std::vector<std::string> args;
 public:
     ReadFile(std::string);
     bool exec(std::vector<std::string>*, std::vector<std::string>*) override;
