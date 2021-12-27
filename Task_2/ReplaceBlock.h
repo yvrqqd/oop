90a9ec250bf5a9eaee06f27be414cf90219dc6c4
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-12-27 08:10:46 +0700

    Update ReplaceBlock.h

--- Task_2/ReplaceBlock.h
+++ Task_2/ReplaceBlock.h
@@ -7,5 +7,5 @@ private:
     std::vector <std::string> args;
 public:
     Replace(const std::string&, const std::string&);
-    bool exec(std::vector<std::string> *input, std::vector<std::string> *output) override;
+    bool exec(std::vector<std::string>*, std::vector<std::string>*) override;
 };
