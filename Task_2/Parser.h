3d21c6cffdb306a2ad291c921c19b7abd9aadfe3
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2021-12-27 07:52:40 +0700

    Create Parser.h

---
+++ Task_2/Parser.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Reader.h"
+#include "GrepBlock.h"
+#include "Replace.h"
+#include "Dump.h"
+#include "Writer.h"
+#include "Sort.h"
+#include "unordered_map"
+
+class Parser {
+public:
+    ifstream& stream;
+    Parser(ifstream&);
+    unordered_map<int, Worker*> commands;
+    Worker* make_block(const string&);
+    void parse_desc();
+    void exec_queue();
+};
