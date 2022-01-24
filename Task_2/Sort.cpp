1c8d219ec2f73d8f0d315ab9c1301d9fb756db5a
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2022-01-24 21:05:10 +0700

    Update Sort.cpp

--- Task_2/Sort.cpp
+++ Task_2/Sort.cpp
@@ -1,16 +1,10 @@
 #include "Sort.h"
 
-bool SortBlock::exec(std::vector<std::string> *input, std::vector<std::string> *output,
-                     bool &contain_input, bool &contain_output, unsigned int index)
-{
-    if(!contain_input)
-        throw logic_error("No input on sort, block index: " + to_string(index));
+bool SortBlock::exec(std::vector<std::string> *input, std::vector<std::string> *output) {
     (*output) = (*input);
     if(input->empty())
         return true;
 
-    sort((*output).begin(), (*output).end(), greater<string>());
-
-    contain_output = true;
+    sort((*output).begin(), (*output).end(), std::greater<std::string>());
     return true;
 }
