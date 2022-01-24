f6c6af89bbea8caded34d2de1724a0e2c112c59e
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2022-01-24 21:11:09 +0700

    Update DumpBlock.cpp

--- Task_2/DumpBlock.cpp
+++ Task_2/DumpBlock.cpp
@@ -1,15 +1,22 @@
-#include "Dump.h"
+#include "DumpBlock.h"
 
-DumpBlock::Dump(std::string filename) {
+DumpBlock::DumpBlock(const std::string& filename) {
     args.push_back(filename);
 }
 
-bool DumpBlock::exec(std::vector<std::string> *input, std::vector<std::string> *output) {
-    ofstream stream(args[0]);
-    for (auto& i : *input){
-        stream<<i<<endl;
+bool
+DumpBlock::exec(std::vector<std::string> *input, std::vector<std::string> *output) {
+    std::ofstream stream(args[0]);
+    if (!stream) {
+        Validator::file_error("File " + args[0] + " not opened!");
+        return false;
+    }
+
+    for (auto &i: *input) {
+        stream << i << std::endl;
         output->push_back(i);
     }
     stream.close();
     return true;
 }
+
