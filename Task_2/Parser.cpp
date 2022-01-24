9a1e5edd720ebb2a1ef5037289889176c8ec8e32
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2022-01-24 21:09:37 +0700

    Update Parser.cpp

--- Task_2/Parser.cpp
+++ Task_2/Parser.cpp
@@ -1,65 +1,64 @@
 #include "Parser.h"
 
+
 Worker *Parser::make_block(const std::string &arg0) {
     std::vector<std::string> args;
     std::string a;
     args.push_back(arg0);
-    switch(args[0]){
-        case "readfile":
-            stream>>a;
-            args.push_back(a);
-            auto* temp = new Reader(args[1]);
-            Worker* prod = temp;
-            return prod;
-            
-        case "writefile":
-            stream>>a;
-            args.push_back(a);
-            auto *temp = new Writer(args[1]);
-            Worker* prod = temp;
-            return prod;
-            
-        case "grep":
-            stream>>a;
-            args.push_back(a);
-            auto* temp = new GrepBlock(args[1]);
-            Worker* prod = temp;
-            return prod;
-            
-        case "sort":
-            auto* temp = new Sort;
-            Worker* prod = temp;
-            return prod;
-            
-        case "replace":
-            stream>>a;
-            args.push_back(a);
-            stream>>a;
-            args.push_back(a);
-            auto *temp = new Replace(args[1],args[2]);
-            Worker* prod = temp;
-            return prod;
-            
-        case args[0] == "dump":
-            stream>>a;
-            args.push_back(a);
-            auto *temp = new Dump(args[1]);
-            Worker* prod = temp;
-            return prod;
-    
-        default:
-            Validator::badFile("Wrong function name. Doesn't match to existing functions.");
+    if (args[0] == "readfile") {
+        stream >> a;
+        args.push_back(a);
+        auto *temp = new Reader(args[1]);
+        Worker *prod = temp;
+        return prod;
+    } else if (args[0] == "writefile") {
+        stream >> a;
+        args.push_back(a);
+        auto *temp = new Writer(args[1]);
+        Worker *prod = temp;
+        return prod;
+    } else if (args[0] == "grep") {
+        stream >> a;
+        args.push_back(a);
+        auto *temp = new GrepBlock(args[1]);
+        Worker *prod = temp;
+        return prod;
+    } else if (args[0] == "sort") {
+        auto *temp = new SortBlock;
+        Worker *prod = temp;
+        return prod;
+    } else if (args[0] == "replace") {
+        stream >> a;
+        args.push_back(a);
+        stream >> a;
+        args.push_back(a);
+        auto *temp = new ReplaceBlock(args[1], args[2]);
+        Worker *prod = temp;
+        return prod;
+    } else if (args[0] == "dump") {
+        stream >> a;
+        args.push_back(a);
+        auto *temp = new DumpBlock(args[1]);
+        Worker *prod = temp;
+        return prod;
+    } else {
+        Validator::file_error("Wrong function name. Doesn't match to existing functions.");
     }
+    return nullptr;
+}
+
+Parser::Parser(std::ifstream& stream1):stream(stream1) {
+
 }
 
 void Parser::parse_desc() {
-    string str;
-    stream>>str;
-    if (str != "desc"){
-        Validator::badFile("No \"desc\", wrong description format");
+    std::string str;
+    stream >> str;
+    if (str != "desc") {
+        Validator::file_error("No \"desc\", wrong description format");
     }
     int index;
-    while (true){
+    while (true) {
         stream >> str;
         if (str == "csed") {
             break;
@@ -67,9 +66,22 @@ void Parser::parse_desc() {
         index = stoi(str);
         stream >> str;
         if (str != "=") {
-            Validator::badFile("Wrong description format.");
+            Validator::file_error("Wrong description format.");
         }
         stream >> str;
         commands[index] = make_block(str);
+    }
+}
+
+void Parser::exec_queue() {
+    std::string str;
+    auto *input = new std::vector<std::string>, *output = new std::vector<std::string>;
+    while (stream >> str) {
+        if (str == "->")
+            continue;
+        commands[std::stoi(str)]->exec(input, output);
+        input->clear();
+        input = output;
+        output = new std::vector<std::string>;
     }
 }
