e37641c82a24bc329126649accd5e7b011dcfe46
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2022-01-24 21:09:12 +0700

    Update Parser.h

--- Task_2/Parser.h
+++ Task_2/Parser.h
@@ -1,19 +1,19 @@
 #pragma once
 
-#include "Reader.h"
-#include "GrepBlock.h"
-#include "Replace.h"
-#include "DumpBlock.h"
-#include "Writer.h"
-#include "Sort.h"
-#include "unordered_map"
+#include "Reader.cpp"
+#include "GrepBlock.cpp"
+#include "ReplaceBlock.cpp"
+#include "DumpBlock.cpp"
+#include "Writer.cpp"
+#include "Sort.cpp"
+#include <unordered_map>
 
 class Parser {
 public:
-    ifstream& stream;
-    Parser(ifstream&);
-    unordered_map<int, Worker*> commands;
-    Worker* make_block(const string&);
+    std::ifstream& stream;
+    Parser(std::ifstream&);
+    std::unordered_map<int, Worker*> commands;
+    Worker* make_block(const std::string&);
     void parse_desc();
     void exec_queue();
 };
