3ea007f469747185b6f3c83cec38a8fecb694397
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2022-01-24 21:02:10 +0700

    Update main.cpp

--- Task_2/main.cpp
+++ Task_2/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-#include "Parser.h"
+#include "Parser.cpp"
 
-int main(int argc; char *argv[]) {
-    ifstream stream("workflow.txt");
-    if (!Validator::file_exists("workflow.txt")){
+int main(int argc, char *argv[]) {
+    std::ifstream stream("../workflow.txt");
+    if (!Validator::file_exists("../workflow.txt")){
         throw std::invalid_argument("workflow.txt does not exist");
     }
-  
     Parser Parser(stream);
     Parser.parse_desc();
     Parser.exec_queue();
 
     return 0;
 }
+
