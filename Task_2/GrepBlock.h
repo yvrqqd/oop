77f6d781e276e0ba1530afbd0dc3e30ddc54205c
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2022-01-24 21:09:54 +0700

    Update GrepBlock.h

--- Task_2/GrepBlock.h
+++ Task_2/GrepBlock.h
@@ -6,8 +6,8 @@ class GrepBlock: public Worker {
 private:
     std::vector <std::string> args;
 public:
-    GrepBlock(std::string);
-    GrepBlock(std::string &&);
-    bool exec(std::vector<std::string>*, std::vector<std::string>* ,
-              bool&, bool&, unsigned int) override;
+    explicit GrepBlock(const std::string&);
+    explicit GrepBlock(std::string &&);
+
+    bool exec(std::vector<std::string>*, std::vector<std::string>*);
 };
